acae7e34434881857667308e222e850e0c793d65
lgachig <163140225+lgachig@users.noreply.github.com>
2026-05-23 12:42:42 -0500

    feat(auth): dockerize auth service with postgres and redis

--- smart-parking/apps/auth-service/src/app/redis/redis.module.ts
+++ smart-parking/apps/auth-service/src/app/redis/redis.module.ts
@@ -1,14 +1,20 @@
 import { Module } from '@nestjs/common';
 import { RedisModule } from '@nestjs-modules/ioredis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    RedisModule.forRoot({
-      type: 'single',
-      url: 'redis://localhost:6379',
+    ConfigModule,
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'single',
+        url: configService.get<string>('REDIS_URL'),
+      }),
     }),
   ],
-
   exports: [RedisModule],
 })
+
 export class AppRedisModule {}
