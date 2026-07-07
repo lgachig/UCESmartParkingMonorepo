aef640ecd085ffaae9ac35f7e633290d998c5f9d
lgachig <163140225+lgachig@users.noreply.github.com>
2026-07-07 15:21:56 -0500

    feat: update welcome message in app service to Hello API

--- smart-parking/apps/ai-service/src/app/app.service.ts
+++ smart-parking/apps/ai-service/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Welcome to ai-service!' };
+    return { message: 'Hello API' };
   }
 }
